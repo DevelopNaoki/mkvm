7c886694d87cc87fd4f066c52da85f85a0e321a6
DevelopNaoki <55051212+DevelopNaoki@users.noreply.github.com>
2023-07-04 04:49:19 +0000

    fix WARN

--- model/model.go
+++ model/model.go
@@ -1,27 +1,25 @@
 package model
 
-import ()
-
 type YamlFile struct {
 	Vm []VM `yaml:"vms"`
 }
 
 type VM struct {
-	Name      string   `yaml:name`
-	Count     int      `yaml:count`
-	Vcpus     int      `yaml:vcpus`
-	Memory    int      `yaml:memory`
-	Disk      Disk     `yaml:disk`
-	Graphics  string   `yaml:graphics`
-	Location  string   `yaml:location`
+	Name      string   `yaml:"name"`
+	Count     int      `yaml:"count"`
+	Vcpus     int      `yaml:"vcpus"`
+	Memory    int      `yaml:"memory"`
+	Disk      Disk     `yaml:"disk"`
+	Graphics  string   `yaml:"graphics"`
+	Location  string   `yaml:"location"`
 	ExtraArgs string   `yaml:"extra-args"`
-	Cdrom     string   `yaml:cdrom`
-	Network   []string `yaml:network`
+	Cdrom     string   `yaml:"cdrom"`
+	Network   []string `yaml:"network"`
 }
 
 type Disk struct {
-	Path       string `yaml:path`
-	Format     string `yaml:format`
-	Size       int    `yaml:size`
-	ImportDisk bool   `yaml:import`
+	Path       string `yaml:"path"`
+	Format     string `yaml:"format"`
+	Size       int    `yaml:"size"`
+	ImportDisk bool   `yaml:"import"`
 }
